54a9b9982fb3681ca7e500bff70bd42202528950
Gareth Fleming <gareth@joocy.com>
2026-06-30 15:49:56 +0100

    add Prefix and Suffix functions

--- strings.go
+++ strings.go
@@ -7,3 +7,13 @@ func Wrap(str string, wrapper string) string {
 	wrappedString := fmt.Sprintf("%v%v%s", wrapper, str, wrapper)
 	return wrappedString
 }
+
+func Prefix(str string, prefix string) string {
+	prefixedString := fmt.Sprintf("%v%v", prefix, str)
+	return prefixedString
+}
+
+func Suffix(str string, suffix string) string {
+	suffixedString := fmt.Sprintf("%v%v", str, suffix)
+	return suffixedString
+}
